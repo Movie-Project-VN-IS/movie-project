0741bec9baf57f0da4f733f4059f229071f56fd3
Irfa Sheikh <irfasheikh@gmail.com>
2021-04-20 15:32:45 -0500

    api-post-successful

--- movie-project.js
+++ movie-project.js
@@ -4,19 +4,13 @@ $(document).ready(function(){
     "use strict"
 
 
-const reviewObj = {
-    restaurant_id: 1,
-    name: 'Codey',
-    rating: 5,
-    comments: "This is a really good place for coding and eating"
-};
 const url = 'https://lyrical-leeward-vinyl.glitch.me/movies';
 const options = {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json',
     },
-    // body: JSON.stringify(reviewObj),
+
 };
 let responses = fetch(url,options)
     responses.then(response => response.json()).then(movies=>console.log(movies))
@@ -24,20 +18,23 @@ let responses = fetch(url,options)
 //how to talk to the form?
 // how does the form talk to the database?
 
-//
-var newMovie = $("#movie-add")
-var newTitle = $("#movie-title-new");
 
-function newMovie(e){
-    e.preventDefault();
-    var addedMovie = {id: coffees.length + 1, name: newCoffeeName.value, roast: newRoast.value};
-    coffees.unshift(addedCoffee);
-    for(var i = 1; i < coffees.length - 1; i++) {
-        if (newCoffeeName.value.toLowerCase() === coffees[i].name.toLowerCase()) {
-            coffees.shift(addedCoffee);
-        }
-    }
-    tbody.innerHTML = renderCoffees(coffees);
+const movieObj = {
+    movie_id: 1,
+    name: 'Bridesmaids',
+    rating: 5,
+    comments: "This is super funny"
+};
+
+
+let addMovie = {
+    method:"POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(movieObj)
 }
-
+fetch(url, addMovie).then(function (response){
+    console.log(response)
+});
 
